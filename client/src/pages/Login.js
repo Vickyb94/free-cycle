b50f09203ea88c49c4efe75f14ca37138964ba81
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-04-27 13:10:47 -0300

    created the login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React from 'react';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <main className="flex-row justify-center mb-8">
+       <div className="col-8 col-lg-4" >
+          <div className="card">
+             <h4 className="card-header bg-blue text-light p-2">Login</h4>
+               <div className="card-body">
+                 <form>
+                   <input
+                     className="form-input"
+                     placeholder="Your email"
+                     name="email"
+                     type="email"
+                     />
+                     <input
+                     className="form-input"
+                     placeholder="*******"
+                     name="password"
+                     type="password"
+                     />
+
+                     <button
+                     className="btn btn-block btn-primary"
+                    style={{ cursor: 'pointer' }}
+                    type="submit">Submit</button>
+                 </form>
+
+
+
+               </div>
+
+
+          </div>
+
+
+       </div>
+
+
+
+    </main>
   )
 }
 
