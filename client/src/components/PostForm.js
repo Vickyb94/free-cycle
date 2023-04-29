4118d6b20cdd5e6288076ddce836d2c2e6785082
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-04-28 23:10:24 -0300

    updated postform

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -1,29 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+
+import { ADD_POST } from '../utils/mutations';
+import { QUERY_POSTS, QUERY_THOUGHTS } from '../utils/queries';
+
+import Auth from '../utils/auth';
 
 const PostForm = () => {
+  const [postText, setPostText] = useState('');
+
+  const [characterCount, setCharacterCount] = useState(0);
+  const [addPost, { error }] = useMutation(ADD_POST, {
+    update(cache, { data: { addPost } }) {
+      try {
+        const { posts } = cache.readQuery({ query: QUERY_POSTS });
+
+        cache.writeQuery({
+          query: QUERY_POSTS,
+          data: { posts: [addPost, ...posts] },
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    },
+  });
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addPost({
+        variables: {
+          thoughtText,
+          thoughtAuthor: Auth.getProfile().data.username,
+        },
+      });
+
+      setThoughtText('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'thoughtText' && value.length <= 280) {
+      setThoughtText(value);
+      setCharacterCount(value.length);
+    }
+  }
   return (
     <div>
       <h3>Do you want to give out something?</h3>
-      <form className="flex-row justify-center justify-space-between-md align-center">
-        <div className="col-12 col-lg-9">
-          <textarea
-            name="postText"
-            placeholder="Here is an item I want to give out..."
-            value="postText"  className="form-input w-100">
-          </textarea>
-          <textarea
-            name="uploadImage"
-            placeholder="Here is an item I want to give out..."
-            value="uploadImage"
-            className="form-input w-100">
-          </textarea>
-          <div className="col-12 col-lg-3">
+
+      {Auth.loggedIn() ? (
+        <>
+          <p
+            className={`m-0 ${characterCount === 280 || error ? 'text-danger' : ''
+              }`}
+          >
+            Character Count: {characterCount}/280
+          </p>
+          <form className="flex-row justify-center justify-space-between-md align-center"
+            onSubmit={handleFormSubmit}>
+
+            <div className="col-12 col-lg-9">
+              <textarea
+                name="postText"
+                placeholder="Here is an item I want to give out..."
+                value={postText} className="form-input w-100"
+                style={{ lineHeight: '1.5', resize: 'vertical' }}
+                onChange={handleChange}>
+              </textarea>
+            </div>
+
+            <div className="col-12 col-lg-3">
               <button className="btn btn-primary btn-block py-3" type="submit">
                 Add Post
               </button>
             </div>
-        </div>
-      </form>
+              {error && (
+                <div className="col-12 my-3 bg-danger text-white p-3">
+                  {error.message}
+                </div>
+              )}
+          </form>
+        </>
+      ) : (
+        <p>
+          You need to be logged in to post. Please{' '}
+          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+        </p>
+      )}
     </div>
   )
 }
