94d6572c686a52638a917ff8797bf5389cba34da
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-04-28 22:23:59 -0300

    added the commentform.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import PostForm from '../components/PostForm'
+import PostForm from '../components/PostForm';
+
+
 
 const Home = () => {
   return (
