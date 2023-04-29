b324ec2e2921064bd3b8fe01ab878f8234c146b6
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-04-28 23:24:48 -0300

    created postlist.js

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const PostList = () => {
+
+const PostList = ({ posts, title }) => {
+  if (!posts.length) {
+      return <h3>No Posts Yet</h3>;
+  }
+
   return (
-    <div>PostList</div>
-  )
-}
+    <div>
+      <h3>{title}</h3>
+      {posts && 
+        posts.map((post) => (
+          <div key={post._id} className="card mb-3">
+            <h4 className="card-header bg-primary text-light p-2 m-0">
+              {post.postAuthor} <br />
+              <span style={{ fontSize: '1rem' }}>
+                commented on this post {post.createdAt}
+              </span>
+            </h4>
+            <div className="card-body bg-light p-2">
+               <p>{post.postText}</p>
+            </div>
+            <Link className="btn btn-primary btn-block btn-squared"
+             to={`/posts/${post._id}`}>
+              Leave a comment on this post
+             </Link>
+          </div>
+        ))
+      }
+    </div>
+  );
+};
 
-export default PostList
+export default PostList;
