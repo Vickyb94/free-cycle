b6c43bc0b7cc7d8afc45073f7f5b0d345a8802f3
Vickyb94 <oluwabusayo.arowosafe@gmail.com>
2023-04-29 20:37:33 -0300

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,75 @@
 import React from 'react';
-import './index.css'
+//import './index.css'
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import SinglePost from "./pages/SinglePost";
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+//Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+   uri: '/graphql',
+});
+
+//Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exits
+   const token = localStorage.getItem('id_token');
+   // return the headers to the context so httpLink can read them
+   return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+   };
+});
+
+const client = new ApolloClient({
+  //Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+   link: authLink.concat(httpLink),
+   cache: new InMemoryCache(),
+});
 
 
-import { Route, Routes } from 'react-router';
 
-const App = () => {
+function App() {
   return (
-  <>
-   <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/login" element={<Login />}/>
-      <Route path="/signup" element={<Signup />}/>
-      <Route path="/singlepost" element={<SinglePost />}/>
-   </Routes>
-  </>
-  );
+    <ApolloProvider client={client}>
+    <Router>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route 
+              path="/" 
+              element={<Home />} 
+            />
+            <Route 
+              path="/login" 
+              element={<Login />} 
+            />
+            <Route 
+              path="/signup" 
+              element={<Signup />} 
+            />
+            <Route 
+              path="/posts/:postId" 
+              element={<SinglePost />} 
+            />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  </ApolloProvider>
+);
 }
 
-export default App
+export default App;
+
